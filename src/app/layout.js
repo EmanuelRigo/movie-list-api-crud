6c8f25220bf49d18e80fae8c596d24c64179a856
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-09-14 00:03:36 -0300

    agregando archivos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="h-screen">
+          <NavBar></NavBar>
+          <div className="mt-8 mx-auto p-4"> {children}</div>
+        </div>
       </body>
     </html>
   );
