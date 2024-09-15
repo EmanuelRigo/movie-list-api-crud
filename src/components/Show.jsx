17ab503cfd7441cd8dff9789d410da7a1cf7a2a1
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2024-09-14 23:55:24 -0300

    commit

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+const getData = async () => {
+  try {
+    const responose = await fetch(process.env.URI, { cache: "no-store" });
+    return responose.json();
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+
 const Show = () => {
   return <div>Show</div>;
 };
